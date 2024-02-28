37e15cc401f32019e4b42da5215d0630e1991a0d
Alain Boudard <alain.boudard@soprasteria.com>
2024-02-28 14:48:14 +0100

    fix: update ng 16

--- projects/ng-app/src/main.ts
+++ projects/ng-app/src/main.ts
@@ -1,10 +1,8 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
@@ -12,6 +10,6 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+    providers: [importProvidersFrom(BrowserModule)]
 })
   .catch(err => console.error(err));
